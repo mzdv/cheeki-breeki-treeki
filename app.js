535ebbb582342f1b0fca7947040a68ed29d1aa3a
Milos Zivadinovic <mzdv@outlook.com>
2015-05-01 01:04:12 +0200

    Made addNode work properly.

--- app.js
+++ app.js
@@ -1,17 +1,40 @@
 var treeTracker = {
-    nextId: 1,
+    currentId: 1,
     numberOfNodes: 0
 };
 
-var nodes = [];
+var treeNodes = [];
 
 var addNode = function(value) {
+    var fatherPosition = Math.floor(treeTracker.currentId / 2) - 1;
+    var father = treeNodes[fatherPosition] ? treeNodes[fatherPosition] : null;
+
     var newNode = {
-        id: treeTracker.id,
+        id: treeTracker.currentId++,
         value: value,
-        previousNode: nodes[treeTracker.nextId - 1] ? nodes[treeTracker.nextId - 1].id : null,
+        previousNode: null,
         leftNode: null,
         rightNode: null,
         freeNodes: 2
     };
+
+    if(father !== null) { // no need to check if freeNodes === 0 since we are using a tracker and cannot remove nodes
+        newNode.previousNode = father.id;
+
+        if(father.freeNodes === 2) {
+            father.leftNode = newNode.id;
+        } else if(father.freeNodes === 1) {
+            father.rightNode = newNode.id;
+        }
+
+        father.freeNodes--;
+    }
+        treeNodes.push(newNode);
+        // this chain of ifs is ugly as hell
 };
+
+addNode(5);
+addNode(3);
+addNode(2);
+
+console.log(treeNodes);
